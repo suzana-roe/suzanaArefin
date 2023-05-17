c542456cc09e26361c4015d74fefc82fae1db3c6
svzana07 <svzana07@gmail.com>
2023-05-17 11:56:26 +0100

    config.php mysqli updated

--- Project2/php/config.php
+++ Project2/php/config.php
@@ -2,14 +2,24 @@
 
 	// connection details for MySQL database
 
-	$cd_host = "127.0.0.1";
-	$cd_port = 3306;
-	$cd_socket = "";
+	//$cd_host = "127.0.0.1";
+	//$cd_port = 3306;
+	//$cd_socket = "";
 
 	// database name, username and password
 
-	$cd_dbname = "companydirectory";
-	$cd_user = "root";
-	$cd_password = "";
+	//$cd_dbname = "companydirectory";
+	//$cd_user = "root";
+	//$cd_password = "";
+
+
+$mysqli = new mysqli("213.171.200.21","suzanaarefin","77.68.64.37","CompanyDirectory");
+
+// Check connection
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+
 
 ?>
