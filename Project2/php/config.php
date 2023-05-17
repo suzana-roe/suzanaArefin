32e4cfc8018eedaec69078b039c61d8843d0a4df
svzana07 <svzana07@gmail.com>
2023-05-17 12:07:02 +0100

    confih.php updated

--- Project2/php/config.php
+++ Project2/php/config.php
@@ -13,13 +13,13 @@
 	//$cd_password = "";
 
 
-$mysqli = new mysqli("213.171.200.21","suzanaarefin","77.68.64.37","CompanyDirectory");
+	$mysqli = new mysqli("213.171.200.21","suzanaarefin","hello1234@","CompanyDirectory");
 
-// Check connection
-if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
-}
+	// Check connection
+	if ($mysqli -> connect_errno) {
+	  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+	  exit();
+	}
 
 
 ?>
