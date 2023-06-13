ba3a86eee9f7d0585a70cc1e3a1bfaa09c908a09
svzana07 <svzana07@gmail.com>
2023-06-13 18:57:17 +0100

    tableHead changes

--- Project1/js/main.js
+++ Project1/js/main.js
@@ -66,7 +66,7 @@ var dark = L.tileLayer(mapboxUrl, {
   accessToken: token,
 }),
 
-streets = L.tileLayer(mapboxUrl, {
+  streets = L.tileLayer(mapboxUrl, {
     id: "mapbox/streets-v11",
     tileSize: 512,
     zoomOffset: -1,
@@ -128,24 +128,26 @@ var landmarks = L.markerClusterGroup({
     weight: 2,
     opacity: 1,
     fillOpacity: 0.5
-  }}).addTo(map);
+  }
+}).addTo(map);
 
-  var airports = L.markerClusterGroup({
-    polygonOptions: {
-      fillColor: '#fff',
-      color: '#000',
-      weight: 2,
-      opacity: 1,
-      fillOpacity: 0.5
-    }}).addTo(map);
+var airports = L.markerClusterGroup({
+  polygonOptions: {
+    fillColor: '#fff',
+    color: '#000',
+    weight: 2,
+    opacity: 1,
+    fillOpacity: 0.5
+  }
+}).addTo(map);
 
 
-  var overlays = {
-    "Airports": airports,
-    "Landmarks": landmarks
-    };
+var overlays = {
+  "Airports": airports,
+  "Landmarks": landmarks
+};
 
-    var layerControl = L.control.layers(baseMaps, overlays).addTo(map);
+var layerControl = L.control.layers(baseMaps, overlays).addTo(map);
 
 
 //var myCircles = new L.featureGroup().addTo(map);
@@ -229,12 +231,12 @@ function getLocationPermission() {
 $("#selCountry").on("change", function () {
   for (const marker of MarkersList) {
     airports.removeLayer(marker),
-    map.removeLayer(airports),
-    landmarks.removeLayer(marker),
-    map.removeLayer(landmarks)
-    
+      map.removeLayer(airports),
+      landmarks.removeLayer(marker),
+      map.removeLayer(landmarks)
+
   }
-  MarkersList= []
+  MarkersList = []
 
   let countryCode = $("#selCountry").val() || selectedCountry;
   //console.log(countryCode, "@country");
@@ -317,7 +319,7 @@ $("#selCountry").on("change", function () {
           //prefix: 'fas'
         });
 
-        const marker = L.marker([airport.lat, airport.lng], {icon:airportMarker})
+        const marker = L.marker([airport.lat, airport.lng], { icon: airportMarker })
         MarkersList.push(airports)
         airports.addLayer(marker)
         //marker.addTo(map);
@@ -327,39 +329,39 @@ $("#selCountry").on("change", function () {
     })
     .catch((error) => console.error(error));
 
-    //loading landmark layer
-    resetCountryData(countryCode).then(
-      (resolved) => {
-        //console.log("capitalCityWeather2: ", capitalCityWeather);
-        //console.log("countryName2: ", countryName);
+  //loading landmark layer
+  resetCountryData(countryCode).then(
+    (resolved) => {
+      //console.log("capitalCityWeather2: ", capitalCityWeather);
+      //console.log("countryName2: ", countryName);
 
-        fetch(
-          `https://nominatim.openstreetmap.org/search.php?q=${capitalCityWeather}%20${countryName}&format=json`
-        )
-          .then((response) => response.json())
-          .then((data) => {
-            //var markers = L.markerClusterGroup();
-            data.forEach((place) => {
-              const PlaceMarker = L.ExtraMarkers.icon({
-                icon: 'fas fa-landmark',
-                markerColor: 'green',
-                shape: 'square',
-                //prefix: 'fas'
-              });
-              const marker = L.marker([place.lat, place.lon], {icon:PlaceMarker})
-              MarkersList.push(landmarks)
-              landmarks.addLayer(marker)
-              //marker.addTo(map);
-              marker.bindPopup(`${place.display_name}`);
+      fetch(
+        `https://nominatim.openstreetmap.org/search.php?q=${capitalCityWeather}%20${countryName}&format=json`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          //var markers = L.markerClusterGroup();
+          data.forEach((place) => {
+            const PlaceMarker = L.ExtraMarkers.icon({
+              icon: 'fas fa-landmark',
+              markerColor: 'green',
+              shape: 'square',
+              //prefix: 'fas'
             });
-            map.addLayer(landmarks);
-          })
-          .catch((error) => {
-            console.error(error);
+            const marker = L.marker([place.lat, place.lon], { icon: PlaceMarker })
+            MarkersList.push(landmarks)
+            landmarks.addLayer(marker)
+            //marker.addTo(map);
+            marker.bindPopup(`${place.display_name}`);
           });
-      },
-      (rejected) => {}
-    );
+          map.addLayer(landmarks);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+    (rejected) => { }
+  );
 });
 
 
@@ -552,14 +554,14 @@ L.easyButton(
           },
           success: function (result) {
 
-            
+
 
             $("#newsList").html("");
-            for (var i = 0; i < result.data.length; i++) 
-            if (result.data[i].author !== null) {
-              
-              $("#newsList")
-                .append(`<div class="newsList"><a href="${result.data[i].url}"> <div class="news-title"><b>${result.data[i].title}</b></div></a>
+            for (var i = 0; i < result.data.length; i++)
+              if (result.data[i].author !== null) {
+
+                $("#newsList")
+                  .append(`<div class="newsList"><a href="${result.data[i].url}"> <div class="news-title"><b>${result.data[i].title}</b></div></a>
           <div class="news-description">${result.data[i].description}
           </div><div class="news-info">
               <div class="news-image news-info-item"><b> </b> <img src=${result.data[i].image}></div>
@@ -569,12 +571,12 @@ L.easyButton(
               <div class="news-country news-info-item"><b>Source: </b>${result.data[i].source}</div>
           </div></div>`);
 
-         
-            }
-            
-            else {
-              $("#newsList")
-                .append(`<div class="newsList"><a href="${result.data[i].url}"> <div class="news-title"><b>${result.data[i].title}</b></div></a>
+
+              }
+
+              else {
+                $("#newsList")
+                  .append(`<div class="newsList"><a href="${result.data[i].url}"> <div class="news-title"><b>${result.data[i].title}</b></div></a>
           <div class="news-description">${result.data[i].description}
           </div><div class="news-info">
               <div class="news-image news-info-item"><b> </b> <img src=${result.data[i].image}></div>
@@ -583,8 +585,8 @@ L.easyButton(
               <div class="news-date news-info-item"><b>Published At: </b>${Date.parse(result.data[i].published_at).toString("MMMM dS, yyyy")}</div>
               <div class="news-country news-info-item"><b>Source: </b>${result.data[i].source}</div>
           </div></div>`);
-            }
-            
+              }
+
           },
           error: function (jqXHR, textStatus, errorThrown) {
             console.log(textStatus, errorThrown);
@@ -614,7 +616,7 @@ L.easyButton(
       data: {},
       success: function (result) {
         //console.log("deaths: ", result.deaths);
-        
+
         $('#covidCountry').html(result.country + "'s" + " " + "Covid Statistics");
         $("#txtCovidDeaths").html("" + numeral(result.deaths).format('0,0') + "<br>");
         $("#txtCovidCases").html(
@@ -643,7 +645,7 @@ L.easyButton(
   '<i class="fas fa-temperature-low fa-1x" id="weatherInfo" data-bs-toggle="modal" data-bs-target="#weatherModal"></i>',
   //'<img src="./img/hot.png" style="height:30px; width:30px;" type="button" id="weatherInfo" data-bs-toggle="modal" data-bs-target="#weatherModal">',
   //'<svg id="weatherInfo" data-bs-toggle="modal" data-bs-target="#weatherModal" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M294.2 1.2c5.1 2.1 8.7 6.7 9.6 12.1l14.1 84.7 84.7 14.1c5.4 .9 10 4.5 12.1 9.6s1.5 10.9-1.6 15.4l-38.5 55c-2.2-.1-4.4-.2-6.7-.2c-23.3 0-45.1 6.2-64 17.1l0-1.1c0-53-43-96-96-96s-96 43-96 96s43 96 96 96c8.1 0 15.9-1 23.4-2.9c-36.6 18.1-63.3 53.1-69.8 94.9l-24.4 17c-4.5 3.2-10.3 3.8-15.4 1.6s-8.7-6.7-9.6-12.1L98.1 317.9 13.4 303.8c-5.4-.9-10-4.5-12.1-9.6s-1.5-10.9 1.6-15.4L52.5 208 2.9 137.2c-3.2-4.5-3.8-10.3-1.6-15.4s6.7-8.7 12.1-9.6L98.1 98.1l14.1-84.7c.9-5.4 4.5-10 9.6-12.1s10.9-1.5 15.4 1.6L208 52.5 278.8 2.9c4.5-3.2 10.3-3.8 15.4-1.6zM144 208a64 64 0 1 1 128 0 64 64 0 1 1 -128 0zM639.9 431.9c0 44.2-35.8 80-80 80H288c-53 0-96-43-96-96c0-47.6 34.6-87 80-94.6l0-1.3c0-53 43-96 96-96c34.9 0 65.4 18.6 82.2 46.4c13-9.1 28.8-14.4 45.8-14.4c44.2 0 80 35.8 80 80c0 5.9-.6 11.7-1.9 17.2c37.4 6.7 65.8 39.4 65.8 78.7z"/></svg>',
-  
+
   function (btn, map) {
     const selectElement = $("#selCountry");
     const selectedIsoCode = $("#selCountry").val()
@@ -670,7 +672,7 @@ L.easyButton(
               "&nbsp;&nbsp;" +
               result.weatherData.weather[0].description + "<br>" +
               "&nbsp;&nbsp;");
-              
+
             $("#feelsLike").html(
               "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; " +
               result.weatherData.main.feels_like +
@@ -697,47 +699,47 @@ L.easyButton(
               "m/s<br>"
             );
 
-            
-         
 
-        //forcast API
-        $.ajax({
-          url: "./php/openWeatherForcast.php",
-          type: 'GET',
-          dataType: 'json',
-          data: {
-              lat: capitalCityLat,
-              lng: capitalCityLon
-          },
-          success: function(result) {
-              
-              //console.log('Weather Forecast',result);
-              
-              if (result.status.name == "ok") {
-                    
-                    $('#forecast-day-1').html(Date.parse(result.weatherForcast.list[8].dt_txt).toString("MMMM dS"));
-                    $('#forecast-day-1-ht').html(Math.floor(result.weatherForcast.list[8].main.temp_max) + '°<br>')
-                    $('#forecast-day-1-lt').html(Math.floor(result.weatherForcast.list[8].main.temp_min) + '°<br>')
 
-                    $('#forecast-day-2-main').html(Date.parse(result.weatherForcast.list[16].dt_txt).toString("MMMM dS"));
-                    $('#forecast-day-2-ht').html(Math.floor(result.weatherForcast.list[16].main.temp_max) + '°<br>')
-                    $('#forecast-day-2-lt').html(Math.floor(result.weatherForcast.list[16].main.temp_min) + '°<br>')
 
-                    $('#forecast-day-3-main').html(Date.parse(result.weatherForcast.list[23].dt_txt).toString("MMMM dS"));
-                    $('#forecast-day-3-ht').html(Math.floor(result.weatherForcast.list[23].main.temp_max) + '°<br>')
-                    $('#forecast-day-3-lt').html(Math.floor(result.weatherForcast.list[23].main.temp_min) + '°<br>')
+            //forcast API
+            $.ajax({
+              url: "./php/openWeatherForcast.php",
+              type: 'GET',
+              dataType: 'json',
+              data: {
+                lat: capitalCityLat,
+                lng: capitalCityLon
+              },
+              success: function (result) {
 
-                    $('#forecast-day-4-main').html(Date.parse(result.weatherForcast.list[33].dt_txt).toString("MMMM dS"));
-                    $('#forecast-day-4-ht').html(Math.floor(result.weatherForcast.list[33].main.temp_max) + '°<br>')
-                    $('#forecast-day-4-lt').html(Math.floor(result.weatherForcast.list[33].main.temp_min) + '°<br>')
+                //console.log('Weather Forecast',result);
+
+                if (result.status.name == "ok") {
+
+                  $('#forecast-day-1').html(Date.parse(result.weatherForcast.list[8].dt_txt).toString("MMMM dS"));
+                  $('#forecast-day-1-ht').html(Math.floor(result.weatherForcast.list[8].main.temp_max) + '°<br>')
+                  $('#forecast-day-1-lt').html(Math.floor(result.weatherForcast.list[8].main.temp_min) + '°<br>')
+
+                  $('#forecast-day-2-main').html(Date.parse(result.weatherForcast.list[16].dt_txt).toString("MMMM dS"));
+                  $('#forecast-day-2-ht').html(Math.floor(result.weatherForcast.list[16].main.temp_max) + '°<br>')
+                  $('#forecast-day-2-lt').html(Math.floor(result.weatherForcast.list[16].main.temp_min) + '°<br>')
+
+                  $('#forecast-day-3-main').html(Date.parse(result.weatherForcast.list[23].dt_txt).toString("MMMM dS"));
+                  $('#forecast-day-3-ht').html(Math.floor(result.weatherForcast.list[23].main.temp_max) + '°<br>')
+                  $('#forecast-day-3-lt').html(Math.floor(result.weatherForcast.list[23].main.temp_min) + '°<br>')
+
+                  $('#forecast-day-4-main').html(Date.parse(result.weatherForcast.list[33].dt_txt).toString("MMMM dS"));
+                  $('#forecast-day-4-ht').html(Math.floor(result.weatherForcast.list[33].main.temp_max) + '°<br>')
+                  $('#forecast-day-4-lt').html(Math.floor(result.weatherForcast.list[33].main.temp_min) + '°<br>')
+                }
+              },
+              error: function (jqXHR, textStatus, errorThrown) {
+                console.log(textStatus, errorThrown);
               }
-          },
-          error: function(jqXHR, textStatus, errorThrown) {
-              console.log(textStatus, errorThrown);
-          }
-      });
+            });
 
-       },
+          },
           error: function (jqXHR, textStatus, errorThrown) {
             console.log(textStatus, errorThrown);
           },
